d1e51fc39ce1a98e696193d751627f771e12f5dd
hideokamoto <hide.okamoto@digitalcube.jp>
2018-01-17 00:27:40 +0900

    init template parts and post loops

--- index.php
+++ index.php
@@ -6,7 +6,12 @@
  */
 
 get_header();
-?>
 
+if ( have_posts() ) {
+	while ( have_posts( ) ) {
+		the_post();
+		get_template_part( 'template-parts/content', get_post_format() );
+	}
+}
 
-<?php get_footer(); ?>
+get_footer();
